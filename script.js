ad107872d3e48ff7059d5d81b89f617218d3634e
ibonzanino <bonzaninoigor@gmail.com>
2022-01-29 21:03:42 -0300

    :construction: frontend done
    frontend fineshed

---
+++ script.js
@@ -0,0 +1,15 @@
+// let times = document.getElementById("times");
+// const launch = new Date(2023, 3, 5, 6, 33, 23)
+// let date = new Date();
+// let diffDate = launch - date;
+// let days = diffDate.getDay() + 1;
+// let hours = diffDate.getHours() + 1;
+// let mins = diffDate.getMinutes() + 1;
+// let secs = diffDate.getSeconds() + 1;
+
+// times.innerHTML = `<p class="time">${days}</p>`
+// times.innerHTML += `<p class="time">${hours}</p>`
+// times.innerHTML += `<p class="time">${mins}</p>`
+// times.innerHTML += `<p class="time">${secs}</p>`
+
+// console.log(times);
